367f24e89c33e8ad8b60c699f12ae0ebfb6c416d
Deevhyn <ogievadivine588@gmail.com>
2025-07-24 06:11:47 +0100

    update

---
+++ Module_1/main.py
@@ -0,0 +1,10 @@
+from crypto_utils import Wallet
+
+my_wallet = Wallet('Divine')
+my_wallet.deposit("ETH", 0.7)
+print(my_wallet.view_balance())
+
+# wallet = CryptoWallet("Joseph")
+# wallet.deposit("ETH", 0.7)
+# wallet.deposit("BTC", 0.1)
+# print(wallet.view_balance())
